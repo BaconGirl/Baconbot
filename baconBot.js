70528f183fd851971af20ea39bd2aa5a424dc23f
BaconGirl <art2006@gmail.com>
2015-09-08 21:50:48 +0200

    startupemoji to false

--- baconBot.js
+++ baconBot.js
@@ -255,7 +255,7 @@
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
-            startupEmoji: true, // true or false
+            startupEmoji: false, // true or false
             autowoot: true,
             autoskip: false,
             smartSkip: true,
