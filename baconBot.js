2f5da0a539c160e8a1f203958c083b494b9008af
BaconGirl <art2006@gmail.com>
2015-09-09 13:49:30 +0200

    added bacon command

--- baconBot.js
+++ baconBot.js
@@ -1910,7 +1910,42 @@
                     }
                 }
             },
-
+            
+             baconCommand: {
+                command: 'bacon',
+                rank: 'user',
+                type: 'startsWith',
+                getBacon: function (chat) {
+                    var c = Math.floor(Math.random() * baconBot.chat.bacon.length);
+                    return baconBot.chat.bacon[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!baconBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(baconBot.chat.eatbacon);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = baconBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(baconBot.chat.nouserbacon, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(baconBot.chat.selfbacon, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(baconBot.chat.bacon, {nameto: user.username, namefrom: chat.un, bacon: this.getBacon()}));
+                            }
+                        }
+                    }
+                }
+            },
+            
             cycleCommand: {
                 command: 'cycle',
                 rank: 'manager',
